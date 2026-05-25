297b09b7b6f02ddfda0b32c48441696703184cc2
sofia villela <sofiavillelav@gmail.com>
2026-05-25 14:47:43 -0300

    fix:sistema de dados errado

--- src/interface/hud.c
+++ src/interface/hud.c
@@ -107,9 +107,5 @@ void desenhar_hud(const EstadoJogo *ej){
     hud_desenhar_barra_vida(ej);
     hud_desenhar_onda(ej);
     hud_desenhar_biomassa(ej);
-    for (int i = 0; i < MAX_DADOS_JOGADOR; i++) {
-        int x = LARGURA_TELA - HUD_MARGEM - DADO_TAMANHO - i * (DADO_TAMANHO + DADO_ESPACO);
-        int y = ALTURA_TELA  - HUD_MARGEM - DADO_TAMANHO;
-        dado_desenhar(&ej->dados_ativos[i], x, y);
-    }
+
 }
--- src/sistemas/dados.c
+++ src/sistemas/dados.c
@@ -9,43 +9,9 @@
 #define DADO_FONTE_SM 12
 
 int dado_rolar(Dado *d) {
-    if (d == NULL || d->faces <= 0) return 0;
-    d->ultimo_resultado = (rand() % d->faces) + 1;
-    return d->ultimo_resultado;
+
 }
 
 void dado_desenhar(const Dado *d, int posicao_x, int posicao_y) {
-    if (d == NULL) return;
 
-    Color cor_borda;
-    switch (d->faces) {
-        case 4:  cor_borda = ORANGE; break;
-        case 6:  cor_borda = WHITE;  break;
-        case 8:  cor_borda = GREEN;  break;
-        case 10: cor_borda = BLUE;   break;
-        case 12: cor_borda = PURPLE; break;
-        case 20: cor_borda = GOLD;   break;
-        default: cor_borda = GRAY;   break;
-    }
-
-    DrawRectangle(posicao_x, posicao_y, DADO_TAMANHO, DADO_TAMANHO, DARKGRAY);
-    DrawRectangleLines(posicao_x, posicao_y, DADO_TAMANHO, DADO_TAMANHO, cor_borda);
-
-    char label_tipo[8];
-    snprintf(label_tipo, sizeof(label_tipo), "d%d", d->faces);
-    DrawText(label_tipo, posicao_x + 3, posicao_y + 3, DADO_FONTE_SM, cor_borda);
-
-    char label_resultado[8];
-    if (d->ultimo_resultado <= 0){
-        snprintf(label_resultado, sizeof(label_resultado), "?");
-    }
-    else{ 
-        snprintf(label_resultado, sizeof(label_resultado), "%d", d->ultimo_resultado);
-    }
-
-    int larg_texto = MeasureText(label_resultado, DADO_FONTE);
-    int centro_x   = posicao_x + (DADO_TAMANHO - larg_texto) / 2;
-    int centro_y   = posicao_y + (DADO_TAMANHO - DADO_FONTE) / 2 + 4;
-
-    DrawText(label_resultado, centro_x, centro_y, DADO_FONTE, WHITE);
 }
